95aa415333424b3f40dbb6a243019206a699f2ce
Dave Dash <dave@pinterest.com>
2014-07-31 17:01:21 -0700

    Maybe routing better?

--- vector.go
+++ vector.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"html/template"
+	"net/http"
 )
 
 type Node struct {
 	Hostname string
 	Services []string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.URL
+	t, _ := template.ParseFiles("templates/home.html")
+	t.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
